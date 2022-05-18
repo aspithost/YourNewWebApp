c8d47f122dd9225d1d97c79c32850dd1de34e790
fissagino <abelspithost@gmail>
2022-05-18 11:58:01 +0200

    contact updated

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="ynwacomp">
         <h1 class="h1-narrow"> Contact</h1>
-        <h2 class="h2-base"> Want to get in touch?</h2>
-        <p class="my-2.5">The easiest way to reach me is to send me an e-mail at info@yournewwebapp.com.</p>
+        <h2 class="h3-base"> Want to get in touch?</h2>
+        <p class="my-2.5">The easiest way to reach me is to head over to my <a href="" class="link">
+            LinkedIn profile</a> and leave me a message. I will try to get back to you as soon as possible.</p>
         <Head>
             <title>Your New Web App - Contact</title>
             <meta name="description" content="Contact Page for Your New Web App">
