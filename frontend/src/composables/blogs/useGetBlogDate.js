4884457bb9254d9768fe6d7050ff45eee39f09df
fissagino <abelspithost@gmail>
2022-05-20 17:49:23 +0200

    date rewrite to day/month/year

--- frontend/src/composables/blogs/useGetBlogDate.js
+++ frontend/src/composables/blogs/useGetBlogDate.js
@@ -1,37 +1,19 @@
 import { ref } from 'vue'
 
 export default () => {
-    
     const writtenDate = ref(null)
-
+    
     const findDate = (date) => {
-
-    console.log('date', date)
-        let rawDate = (new Date().valueOf() - new Date(date).valueOf()) / (1000 * 60)
-        if (rawDate < 0) {
-            writtenDate.value = date
-            return
-        }
-        if (rawDate < 1) {
-            writtenDate.value = 'seconds ago'
-        } else if (rawDate < 2) {
-            writtenDate.value = 'one minute ago'
-        } else if (rawDate < 60) {
-            writtenDate.value = `${Math.floor(rawDate)} minutes ago`
-        } else if (rawDate < 120) {
-            writtenDate.value = 'one hour ago'
-        } else if (rawDate < 1440) {
-            writtenDate.value = `${Math.floor(rawDate/60)} hours ago`
-        } else if (rawDate < 2880) {
-            writtenDate.value = 'one day ago'
-        } else if (rawDate < 10080) {
-            writtenDate.value = `${Math.floor(rawDate/1440)} days ago`
-        } else if (rawDate < 20160) {
-            writtenDate.value = 'one week ago'
+        let localDate = new Date().toISOString().slice(0, 10)
+        if (localDate === date.slice(0,10)) {
+            writtenDate.value = 'Today'
         } else {
-            writtenDate.value = `${Math.floor(rawDate/10080)} weeks ago`
+            writtenDate.value = new Date(date)
+                .toLocaleString('en-US', { 
+                    day: 'numeric', month: 'long', year: 'numeric' 
+                })
         }
-    }        
+    }
 
     return { writtenDate, findDate }
 }
