b0cb66c2180b56f8efb75af8e93d539c9b956104
fissagino <abelspithost@gmail>
2022-05-21 20:44:19 +0200

    update frontend

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -2,9 +2,11 @@
     <div class="ynwacomp">
         <h1 class="h1-narrow"> Contact</h1>
         <h2 class="h3-base"> Want to get in touch?</h2>
-        <p class="my-2.5">The easiest way to reach me is to head over to my <a href="https://linkedin.com/in/abelspithost" 
+        <p>The easiest way to reach me is to head over to my <a href="https://linkedin.com/in/abelspithost" 
             target="_blank" class="link">
-            LinkedIn profile</a> and leave me a message. I will try to get back to you as soon as possible.</p>
+            LinkedIn profile</a> and leave me a message. I will try to get back to you as soon as possible!</p>
+        <p>If you would like to have a look at all of Your New Web App's code, head on over to my 
+            <a href="https://github.com/aspithost/" target="_blank" class="link">public Github repo</a>.</p>
         <Head>
             <title>Your New Web App - Contact</title>
             <meta name="description" content="Contact Page for Your New Web App">
