d81f6255d2b89637448786f1c995f552cfcc1a26
fissagino <abelspithost@gmail>
2022-05-24 21:42:15 +0200

    router position fix

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,15 +8,16 @@ export default function createRouter(store){
     return _createRouter({
         history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
         scrollBehavior (to, from, savedPosition) {
+            // Prevent Router behavior when scrolling homepage
             if ((from.hash || from.name) && to.name === 'Home') {
                 return
-            } else {
+            } else if (savedPosition && savedPosition.top) {
                 return new Promise (resolve => {
                     setTimeout(() => {
                         resolve({ top: savedPosition.top })
-                    }, 100)
+                    }, 200)
                 })
-            }
+            } 
         },
         routes
     })
