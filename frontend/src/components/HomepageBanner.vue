0050478a909fb223d05fc9c167366dc771630e52
fissagino <abelspithost@gmail>
2022-05-24 14:28:03 +0200

    homepage banner updated

--- frontend/src/components/HomepageBanner.vue
+++ frontend/src/components/HomepageBanner.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="bg-[#1d4680] flex flex-col h-[400px] md:h-[500px] px-4"> 
-        <h1 class="text-white m-auto text-center font-header text-3xl md:text-4xl font-extrabold">
-            Helping You Build Your New Web App.
-        </h1>
-        <p class="text-white mx-auto pb-10 md:pb-20 text-center text-2xl md:text-3xl font-header">
-            Let my experiences building my web app help you build yours.
-        </p>
-        <div class="flex flex-wrap justify-center mb-6 md:mb-10">
+    <div class="bg-[#FFAE4E] flex flex-col h-[400px] px-4"> 
+        <div class="flex flex-col text-center justify-between items-center h-full">
+            <h1 class="text-white font-header text-3xl md:text-4xl font-extrabold mt-28">
+                Helping You Build Your New Web App.
+            </h1>
+            <span class="text-white text-2xl md:text-3xl font-header mb-12 md:mb-20">
+                Let my experiences building my web app help you build yours.
+            </span>
+        </div>
+        <div class="flex flex-wrap justify-center mb-6 md:mb-8">
             <a
                 v-for="(button, index) in ['Get Coding!', 'About YNWA', 'About Me']" 
                 @click="emit('clicked', `#${button.toLowerCase().replace(' ', '-').replace('!', '')}`)"
