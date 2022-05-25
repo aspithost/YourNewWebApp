27bac1d61c4e5803df63ef4f54a450f2b31ad0cf
fissagino <abelspithost@gmail>
2022-05-25 10:49:03 +0200

    router fix homepage

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,7 +9,8 @@ export default function createRouter(store){
         history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
         scrollBehavior (to, from, savedPosition) {
             // Prevent Router behavior when scrolling homepage
-            if ((from.hash || from.name) && to.name === 'Home') {
+            console.log('route')
+            if (to.name === 'Home') {
                 return
             } else if (savedPosition && savedPosition.top) {
                 return new Promise (resolve => {
