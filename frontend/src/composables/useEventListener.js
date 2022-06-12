6738f620a74091ba494d23581bfe0a7eb0a27213
fissagino <abelspithost@gmail>
2022-06-12 12:02:14 +0200

    explanation added

--- frontend/src/composables/useEventListener.js
+++ frontend/src/composables/useEventListener.js
@@ -1,5 +1,6 @@
 import { onUnmounted } from 'vue'
 
+// Call onMounted within components themselves to have access to the document/window objects
 export default (target, event, callback) => {
     target.addEventListener(event, callback)
     onUnmounted(() => target.removeEventListener(event, callback))
